5a6a383951b37672d3f71dc6a05d97f70ffc26c1
vianviet <84424817+vj3t99@users.noreply.github.com>
2021-12-18 01:58:11 +0700

    Update MailConfig.java

--- src/main/java/com/musical16/config/MailConfig.java
+++ src/main/java/com/musical16/config/MailConfig.java
@@ -25,6 +25,10 @@ public class MailConfig {
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
+	props.put("mail.smtp.socketFactory.fallback", "true");
+	props.put("mail.smtp.socketFactory.port", "587");
+	props.put("mail.smtp.starttls.required", "true");
+	props.put("mail.smtp.ssl.enable", "false");
 
         return mailSender;
 	}
